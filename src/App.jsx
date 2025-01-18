72e870c4683b8ef45608e2e9ad0519d8e1b4bf0e
vladlapkovski <vlad.lapkofffskii@gmail.com>
2025-01-18 08:30:24 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
